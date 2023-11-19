beb9fd83d8eebad96ea3074162130b2858a634d6
vdegenne <vdegenne@gmail.com>
2023-11-19 12:39:02 +0100

    make koa bundlable

--- packages/vscode-ui-connector/rollup.config.js
+++ packages/vscode-ui-connector/rollup.config.js
@@ -1,5 +1,7 @@
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import cjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
 
 /** @type {import('rollup').RollupOptions} */
 export default {
@@ -8,9 +10,12 @@ export default {
 		file: 'dist/index.js',
 	},
 	plugins: [
-		nodeResolve({
-			resolveOnly: ['shared'],
-		}),
+		cjs(),
+		json(),
+		// nodeResolve({
+		// 	resolveOnly: ['shared'],
+		// }),
+		nodeResolve(),
 		terser(),
 	],
 };
